da048eb44e425829f5895fa00c47016b807d6aa7
Dejan <dejan.milovanovic@codeup.rocks>
2021-06-08 22:19:22 +0200

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-mobile-manufacturers
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_mobile_manufacturers/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-mobile-manufacturers.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-mobile-manufacturers.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
